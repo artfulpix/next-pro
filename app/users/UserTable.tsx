631d18ed8ae1704c710a0644372353015a620f30
Artfulpix <aliciajunn@gmail.com>
2023-11-05 16:29:12 +0300

    sorted the users table on the server

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,8 +1,3 @@
-// "use client";
-
-// import React, { useEffect, useState } from "react";
-// import React, { useEffect, useState } from "react";
-
 import { sort } from "fast-sort";
 
 import { User, UserSortBy } from "./page";
@@ -23,46 +18,11 @@ const UserTable = ({ sortBy, users }: Props) => {
       ? u.id
       : u
   );
-  // const [sortedBy, setSortBy] = useState<UserSortBy | "">(sortBy);
-  // const [sortedUsers, setSortedUsers] = useState<User[]>([]);
-
-  // console.log(sortedBy);
-
-  // useEffect(() => {
-  //   setSortedUsers(() =>
-  //     sort(users).asc((u) =>
-  //       sortedBy === "name"
-  //         ? u.name
-  //         : sortedBy === "email"
-  //         ? u.email
-  //         : sortedBy === "id"
-  //         ? u.id
-  //         : u
-  //     )
-  //   );
-  // }, [sortedBy]);
-
-  // useEffect(() => {
-  //   sortedBy === "name"
-  //     ? setSortedUsers(() => sort(users).asc((u) => u.name))
-  //     : sortedBy === "email"
-  //     ? setSortedUsers(() => sort(users).asc((u) => u.email))
-  //     : sortedBy === "id"
-  //     ? setSortedUsers(() => sort(users).asc((u) => u.id))
-  //     : setSortedUsers(users);
-  // }, [sortedBy]);
-
-  // const setSort = (sortBy: UserSortBy) =>
-  //   sortBy !== sortedBy ? setSortBy(sortBy) : setSortBy("");
 
   return (
     <table className="tw-table tw-table-zebra-zebra">
       <thead>
         <tr>
-          {/* <th onClick={() => setSort("id")}>Id</th>
-          <th onClick={() => setSort("name")}>Name</th>
-          <th onClick={() => setSort("email")}>Email</th> */}
-
           <th>
             <Link href="/users?sortBy=id">Id</Link>{" "}
           </th>
